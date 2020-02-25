cdf18e5405e898f7c5dd1a37ee09f6a781c3beee
sandreza <andrenogueirasouza@gmail.com>
2020-02-25 11:06:26 -0800

    minor changes

--- CalTech/diffusion.jl
+++ CalTech/diffusion.jl
@@ -5,7 +5,7 @@ include("diffusion_utils.jl")
 using Plots, DifferentialEquations, JLD2, Printf
 
 # Mesh Stuff
-K = 8     # Number of elements
+K = 16     # Number of elements
 n = 2      # Polynomial Order
 xmin = 0.0 # left endpoint of domain
 xmax = 2π  # right endpoint of domain
@@ -16,7 +16,7 @@ xmax = 2π  # right endpoint of domain
 u = @. exp(-2 * (xmax-xmin) / 3 * (𝒢.x - (xmax-xmin)/2)^2)
 
 # Define hyperbolic flux
-α = 1.0 # Rusanov prameter
+α = 0.0 # Rusanov prameter
 flux_type = Rusanov(α)
 field_bc = Dirichlet(0.0, 1.0)
 field_data = copy(u)
