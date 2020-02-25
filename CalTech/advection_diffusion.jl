6c6aaa97659ed19232b1924719943d5125f98368
sandreza <andrenogueirasouza@gmail.com>
2020-02-25 09:58:57 -0800

    advection diffusion

---
+++ CalTech/advection_diffusion.jl
@@ -0,0 +1,70 @@
+include("../dg_utils/data_structures.jl")
+include("../dg_utils/mesh.jl")
+include("advection_diffusion_utils.jl")
+
+using Plots, DifferentialEquations, JLD2, Printf
+
+# Mesh Stuff
+K = 16     # Number of elements
+n = 2      # Polynomial Order
+xmin = 0.0 # left endpoint of domain
+xmax = 2π  # right endpoint of domain
+𝒢 = Mesh(K, n, xmin, xmax) # Generate Mesh
+∇ = Gradient(𝒢)
+
+# Define Initial Condition
+u = @. exp(-2 * (xmax-xmin) / 3 * (𝒢.x - (xmax-xmin)/2)^2)
+
+# Define hyperbolic flux
+α = 1.0 # Rusanov prameter
+flux_type = Rusanov(α)
+field_bc = Periodic()
+field_data = copy(u)
+flux_field = Field(field_data, field_bc)
+state = copy(u)
+Φ = Flux(flux_type, flux_field, state, calculate_hyperbolic_flux)
+
+# Define Diffusive flux
+α = 0.0 # Rusanov parameter
+flux_type = Rusanov(α)
+field_bc = Periodic()
+field_data = copy(u)
+flux_field = Field(field_data, field_bc)
+state = copy(u)
+∇Φ = Flux(flux_type, flux_field, state, calculate_parabolic_flux)
+
+# Define Advective flux
+α = -1.0 # Rusanov parameter (negative)
+flux_type = Rusanov(α)
+field_bc = Periodic()
+field_data = copy(u)
+flux_field = Field(field_data, field_bc)
+state = copy(u)
+𝒜Φ = Flux(flux_type, flux_field, state, calculate_advective_flux)
+
+# Define Diffusion parameters
+dt = cfl_advection_diffusion(𝒢, c) # CFL timestep
+tspan  = (0.0, 2.0)
+params = (∇, Φ, ∇Φ, 𝒜Φ)
+rhs! = advection_diffusion!
+
+# Define ODE problem
+prob = ODEProblem(rhs!, u, tspan, params);
+# Solve it
+sol  = solve(prob, Tsit5(), dt=dt, adaptive = false);
+
+# Plot it
+theme(:juno)
+nt = length(sol.t)
+num = 20 # Number of Frames
+step = floor(Int, nt/num)
+num = floor(Int, nt/step)
+indices = step * collect(1:num)
+pushfirst!(indices, 1)
+push!(indices, nt)
+for i in indices
+    plt = plot(𝒢.x, sol.u[i], xlims=(xmin, xmax), ylims = (-0.1,1.1), marker = 3,    leg = false)
+    plot!(𝒢.x, sol.u[1], xlims = (xmin, xmax), ylims = (-0.1,1.1), color = "red", leg = false, grid = true, gridstyle = :dash, gridalpha = 0.25, framestyle = :box)
+    display(plt)
+    # sleep(0.25)
+end
---
+++ CalTech/advection_diffusion_utils.jl
@@ -0,0 +1,65 @@
+include("../dg_utils/data_structures.jl")
+const c = 2π
+const κ = 0.1
+
+function calculate_hyperbolic_flux(x::AbstractArray)
+    return κ .* x
+end
+
+function calculate_parabolic_flux(x::AbstractArray)
+    return κ .* x
+end
+
+function calculate_hyperbolic_flux(x::Number)
+    return x
+end
+
+function calculate_parabolic_flux(x::Number)
+    return x
+end
+
+function calculate_advective_flux(x::AbstractArray)
+    return -c .* x
+end
+
+function calculate_advective_flux(x::Number)
+    return -c * x
+end
+
+# Define right hand side of the differential equation
+function advection_diffusion!(u̇, u, params, t)
+    # unpack params
+    ∇ = params[1]           # Gradient operator
+    Φ = params[2]           # flux term
+    ∇Φ = params[3]          # diffusive state
+    𝒜Φ = params[4]         # advection term
+    Φ.state .= u            # update state
+    𝒜Φ.state .= u           # update advective state
+    q = ∇⋅Φ                 # calculate gradient
+    ∇Φ.state .= q           # store gradient
+    tmp =  ∇⋅∇Φ             # calculate tendency
+    tmp += ∇⋅𝒜Φ             # add in advective contribution
+    @. u̇ = tmp              # store it
+    return nothing
+end
+
+# Determine timestep
+function cfl_diffusive(𝒢, c; α = c, CFL = 0.1)
+    Δx  = minimum(𝒢.x[2,:] -𝒢.x[1,:])
+    dt  = CFL * Δx^2 / maximum([α, c])
+    return dt
+end
+
+# Determine timestep
+function cfl(𝒢, c; α = c, CFL = 0.75)
+    Δx  = minimum(𝒢.x[2,:] -𝒢.x[1,:])
+    dt  = CFL * Δx / maximum([α, c])
+    dt *= 0.5
+    return dt
+end
+
+# Determine timestep
+
+function cfl_advection_diffusion(𝒢, c; α = c, CFL = 0.1)
+    return minimum([cfl(𝒢, c; α = α, CFL = CFL), cfl_diffusive(𝒢, c; α = α, CFL = CFL)])
+end
