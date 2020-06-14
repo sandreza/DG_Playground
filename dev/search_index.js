7b4852a101591ffc4674abfd78ae2f3436d95489
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-06-14 05:32:08 +0000

    build based on eaf0d39

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DG_Playground.jl-1","page":"Home","title":"DG_Playground.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"Documentation for DG_Playground.jl","category":"page"}]
+}
