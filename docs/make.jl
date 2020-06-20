20c46da5f9379d2a72c70ae109ecd4a1fca85997
sandreza <andrenogueirasouza@gmail.com>
2020-06-20 01:30:05 -0400

    documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,24 @@
 using Documenter
 using DG_Playground
 
+
+dg_methods = Any[
+    "Home" => "dg_methods.md",
+    "Single Element" => "dg_single_element.md",
+]
+
+physics = Any[
+    "Home" => "physics.md",
+    "Convective Adjustment" => "convective_adjustment.md",
+]
+
 makedocs(
     sitename = "DG_Playground",
-    format = Documenter.HTML(),
+    format = Documenter.HTML(collapselevel = 1),
     pages = [
     "Home" => "index.md",
-    "Discontinuous Galerkin" => "dg_methods.md",
-    "Convective Adjustment" => "convective_adjustment.md",
+    "Discontinuous Galerkin" => dg_methods,
+    "Physics" => physics,
     "Function Index" => "function_index.md",
     ],
     modules = [DG_Playground]
