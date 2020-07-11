252d0f0206a007bd10da9af83dc9d3270b7c55bd
Thomas Gibson <gibsonthomas1120@hotmail.com>
2020-07-11 11:35:10 -0700

    WIP: Allow constructions of fields using arrays of scalars

--- Equations/Fields.jl
+++ Equations/Fields.jl
@@ -2,22 +2,48 @@
 export AbstractTensorField
 export ScalarField, VectorField, MatrixField
 
-abstract type AbstractTensorField{R} <: Terminal end
+abstract type AbstractTensorField{Rank} <: Terminal end
 
+struct FieldBase{R}
+    shape::Tuple
+    fields::AbstractArray{AbstractTensorField{0}, R}
+end
 
 struct ScalarField{S, T} <: AbstractTensorField{0}
     metadata::S
-    data::T
+    function ScalarField(; metadata = nothing)
+        return new{typeof(metadata)}(metadata)
+    end
 end
 
 struct VectorField{S, T} <: AbstractTensorField{1}
     metadata::S
     data::T
+    function VectorField(
+        fields::AbstractArray{AbstractTensorField{0}, 1};
+        metadata = nothing,
+        data = nothing,
+    )
+        data = FieldBase{1}(size(fields), fields)
+        return new{typeof(metadata),
+                   typeof(data)}(metadata, data)
+    end
 end
 
 struct MatrixField{S, T} <: AbstractTensorField{2}
     metadata::S
     data::T
+    function MatrixField(
+        fields::AbstractArray{AbstractTensorField{0}, 2};
+        metadata = nothing,
+        data = nothing,
+    )
+        data = FieldBase{2}(size(fields), fields)
+        return new{typeof(metadata),
+                   typeof(data)}(metadata, data)
+    end
 end
 
 rank(::AbstractTensorField{T}) where T = T
+shape(::ScalarField) = ()
+shape(o::Union{VectorField, MatrixField}) = o.data.shape
